30d0abe205eaf93f51b5d598ef2363ba90fe0838
bradleyjsimons <simons@ualberta.ca>
2014-02-04 19:21:01 -0700

    camera

--- src/ca/ualberta/cs/comput301/currentlocation/CurrentLocationActivity.java
+++ src/ca/ualberta/cs/comput301/currentlocation/CurrentLocationActivity.java
@@ -1,38 +1,68 @@
 package ca.ualberta.cs.comput301.currentlocation;
 
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Context;
 import android.view.Menu;
 import android.widget.TextView;
 
 public class CurrentLocationActivity extends Activity {
-	private static final String GPS_MOCK_PROVIDER = "GpsMockProvider";
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		
-		// Obtain LocationManager service and set up location update request.
-		// To Do
-	}
+    private static final String MOCK_LOCATION_PROVIDER = "mockLocationProvider";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	
-	// Retrieve location updates through LocationListener interface
-	private final LocationListener locationListener = new LocationListener(){
-	// To Do: override the four methods.
-	}	
 
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        // Obtain LocationManager service and set up location update request.
+        // To Do
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(MOCK_LOCATION_PROVIDER, 1000, 0, locationListener);
+    }
+
+
+    // Retrieve location updates through LocationListener interface
+    private final LocationListener locationListener = new LocationListener(){
+        // To Do: override the four methods.
+        public void onLocationChanged (Location location) {
+            TextView tv = (TextView) findViewById(R.id.myLocationText);
+            if (location != null) {
+                double lat = location.getLatitude();
+                double lng = location.getLongitude();
+                Date date = new Date();
+                tv.setText("Your Location is: "
+                        + "\nLatitude: " + lat 
+                        + "\nLongitude: " + lng
+                        + "\nat: " +date.toString());
+            } else {
+                tv.setText("NO location information");
+            }
+        } 
+        
+        public void onProviderDisabled (String provider) {
+            
+        }
+        
+        public void onProviderEnabled (String provider) {
+            
+        }
+        
+        public void onStatusChanged (String provider, int status, Bundle extras) {
+            
+        }
+    };
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
